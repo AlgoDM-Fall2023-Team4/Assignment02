5c3fcb8c8f7053c14ac697af859e3e96f530251e
PranithaPoosa <114799068+PranithaPoosa@users.noreply.github.com>
2023-10-19 10:58:57 -0400

    changes in main.py

--- streamlit/main.py
+++ streamlit/main.py
@@ -7,7 +7,7 @@ import calendar
 import altair as alt
 import streamlit as st
 import pandas as pd
-import snowflake.snowpark as snowpark
+import numpy as np
 from snowflake.snowpark.functions import col
 
 load_dotenv()
@@ -30,16 +30,39 @@ def load():
 def predict(budgets):
   pred = session.sql(f"SELECT predict_roi(array_construct({budgets[0]*1000},{budgets[1]*1000},{budgets[2]*1000},{budgets[3]*1000})) as PREDICTED_ROI").to_pandas()
   pred = pred["PREDICTED_ROI"].values[0] / 100000
-  change = round(((pred / rois["ROI"].iloc[-1]) - 1) * 100, 1)
+  change = np.round(((pred / rois["ROI"].iloc[-1]) - 1) * 100, 1)
+  st.write(change)
   return pred, change
 
 def chart(chart_data):
-  base = alt.Chart(chart_data).encode(alt.X("MONTH", sort=list(calendar.month_name), title=None))
-  bars = base.mark_bar().encode(y=alt.Y("BUDGET", title="Budget", scale=alt.Scale(domain=[0, 300])), color=alt.Color("CHANNEL", legend=alt.Legend(orient="top", title=" ")), opacity=alt.condition(alt.datum.MONTH=="July", alt.value(1), alt.value(0.3)))
-  lines = base.mark_line(size=3).encode(y=alt.Y("ROI", title="Revenue", scale=alt.Scale(domain=[0, 25])), color=alt.value("#808495"))
-  points = base.mark_point(strokeWidth=3).encode(y=alt.Y("ROI"), stroke=alt.value("#808495"), fill=alt.value("white"), size=alt.condition(alt.datum.MONTH=="July", alt.value(300), alt.value(70)))
-  chart = alt.layer(bars, lines + points).resolve_scale(y="independent").configure_view(strokeWidth=0).configure_axisY(domain=False).configure_axis(labelColor="#808495", tickColor="#e6eaf1", gridColor="#e6eaf1", domainColor="#e6eaf1", titleFontWeight=600, titlePadding=10, labelPadding=5, labelFontSize=14).configure_range(category=["#FFE08E", "#03C0F2", "#FFAAAB", "#995EFF"])
-  st.altair_chart(chart, use_container_width=True)
+    base = alt.Chart(chart_data).encode(
+        alt.X("MONTH", sort=list(calendar.month_name), title=None)
+    )
+
+    bar_chart = base.mark_bar().encode(
+        y=alt.Y('BUDGET', title='Budget', scale=alt.Scale(domain=[0, 300])),
+        color=alt.Color('CHANNEL', legend=alt.Legend(orient="top", title=" ")),
+        opacity = alt.condition(alt.datum.MONTH == "July", alt.value(1), alt.value(0.3))
+    ).properties(
+        width=600
+    )
+
+    line_chart = base.mark_line(size=3).encode(
+        y=alt.Y('ROI', title='Revenue', scale=alt.Scale(domain=[0, 25])),
+        color=alt.value('#808495')
+    )
+
+    points = base.mark_point(strokeWidth=3).encode(
+        y=alt.Y('ROI'),
+        stroke = alt.value("#808495"),
+        fill = alt.value("white"),
+        size = alt.condition(alt.datum.MONTH == "July", alt.value(300), alt.value(70))
+    )
+
+    # Combine the three charts
+    combined_chart = alt.layer(bar_chart, line_chart + points).resolve_scale(y="independent").configure_view(strokeWidth=0).configure_axisY(domain=False).configure_axis(labelColor="#808495", tickColor="#e6eaf1", gridColor="#e6eaf1", domainColor="#e6eaf1", titleFontWeight=600, titlePadding=10, labelPadding=5, labelFontSize=14).configure_range(category=["#FFE08E", "#03C0F2", "#FFAAAB", "#995EFF"])
+    st.altair_chart(combined_chart)
+
 
 # Streamlit config
 st.header("SkiGear Co Ad Spend Optimizer")
@@ -62,9 +85,10 @@ for alloc, col in zip(last_alloc.itertuples(), [col1, col1, col2, col2]):
 pred, change = predict(budgets)
 st.metric("", f"Predicted revenue ${pred:.2f} million", f"{change:.1f} % vs last month")
 july = pd.DataFrame({"MONTH": ["July"]*4, "CHANNEL": channels_upper, "BUDGET": budgets, "ROI": [pred]*4})
-chart(data.append(july).reset_index(drop=True).replace(channels_upper, channels))
+merged_df = pd.concat([data,july])
+st.write(merged_df.reset_index(drop=True).replace(channels_upper, channels))
+chart(merged_df.reset_index(drop=True).replace(channels_upper, channels))
 
-# Setup the ability to save user-entered allocations and predicted value back to Snowflake
 if st.button("❄️ Save to Snowflake"):
   with st.spinner("Making snowflakes..."):
     df = pd.DataFrame({"MONTH": ["July"], "SEARCHENGINE": [budgets[0]], "SOCIALMEDIA": [budgets[1]], "VIDEO": [budgets[2]], "EMAIL": [budgets[3]], "ROI": [pred]})
