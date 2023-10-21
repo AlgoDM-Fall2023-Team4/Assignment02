96ecdd89cf48e4bd2a581a1ab1c5fc01b296938f
PranithaPoosa <114799068+PranithaPoosa@users.noreply.github.com>
2023-10-21 13:47:45 -0400

    pcs and forecast models

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,15 +1,18 @@
 # %%
 import streamlit as st
-import roi
+import roi, forecast_anomaly_detection,pcs
 
 st.sidebar.title("Navigation")
 page = st.sidebar.radio("Go to", ["HOME", "CLV", "PCS", "ROI"])
 
-if page == "CLV":
+if page == "HOME":
+    forecast_anomaly_detection.main()
+elif page == "CLV":
     # clv_model()
     pass
-elif page == "HOME":
-    # homepage()
+elif page == "PCS":
+    pcs.main()
+    # pcs_model()
     pass
 elif page == "ROI":
     roi.main()
